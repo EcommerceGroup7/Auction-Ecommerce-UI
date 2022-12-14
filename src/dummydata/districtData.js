3423e65210c32a77845bbca7c286a24a8da4737b
killerburn69 <kietthetran123@gmail.com>
2022-12-14 23:36:32 +0700

    fix dummy data

--- src/dummydata/districtData.js
+++ src/dummydata/districtData.js
@@ -1,98 +1,98 @@
-export const districtData = [
-    {
-        id:"1",
-        district:"District 1"
-    },
-    {
-        id:"2",
-        district:"District 2"
-    },
-    {
-        id:"3",
-        district:"District 3"
-    },
-    {
-        id:"4",
-        district:"District 4"
-    },
-    {
-        id:"5",
-        district:"District 5"
-    },
-    {
-        id:"6",
-        district:"District 6"
-    },
-    {
-        id:"7",
-        district:"District 7"
-    },
-    {
-        id:"8",
-        district:"District 8"
-    },
-    {
-        id:"9",
-        district:"District 9"
-    },
-    {
-        id:"10",
-        district:"District 10"
-    },
-    {
-        id:"11",
-        district:"District 11"
-    },
-    {
-        id:"12",
-        district:"District 12"
-    },
-    {
-        id:"13",
-        district:"Thu Duc"
-    },
-    {
-        id:"14",
-        district:"Phu Nhuan"
-    },
-    {
-        id:"15",
-        district:"Tan Binh"
-    },
-    {
-        id:"16",
-        district:"Binh Thanh"
-    },
-    {
-        id:"17",
-        district:"Go Vap"
-    },
-    {
-        id:"18",
-        district:"Tan Phu"
-    },
-    {
-        id:"19",
-        district:"Binh Tan"
-    },
-    {
-        id:"20",
-        district:"Binh Chanh"
-    },
-    {
-        id:"21",
-        district:"Hoc Mon"
-    },
-    {
-        id:"22",
-        district:"Cu Chi"
-    },
-    {
-        id:"23",
-        district:"Can Gio"
-    },
-    {
-        id:"24",
-        district:"Nha Be"
-    },
-]
+// export const districtData = [
+//     {
+//         id:"1",
+//         district:"District 1"
+//     },
+//     {
+//         id:"2",
+//         district:"District 2"
+//     },
+//     {
+//         id:"3",
+//         district:"District 3"
+//     },
+//     {
+//         id:"4",
+//         district:"District 4"
+//     },
+//     {
+//         id:"5",
+//         district:"District 5"
+//     },
+//     {
+//         id:"6",
+//         district:"District 6"
+//     },
+//     {
+//         id:"7",
+//         district:"District 7"
+//     },
+//     {
+//         id:"8",
+//         district:"District 8"
+//     },
+//     {
+//         id:"9",
+//         district:"District 9"
+//     },
+//     {
+//         id:"10",
+//         district:"District 10"
+//     },
+//     {
+//         id:"11",
+//         district:"District 11"
+//     },
+//     {
+//         id:"12",
+//         district:"District 12"
+//     },
+//     {
+//         id:"13",
+//         district:"Thu Duc"
+//     },
+//     {
+//         id:"14",
+//         district:"Phu Nhuan"
+//     },
+//     {
+//         id:"15",
+//         district:"Tan Binh"
+//     },
+//     {
+//         id:"16",
+//         district:"Binh Thanh"
+//     },
+//     {
+//         id:"17",
+//         district:"Go Vap"
+//     },
+//     {
+//         id:"18",
+//         district:"Tan Phu"
+//     },
+//     {
+//         id:"19",
+//         district:"Binh Tan"
+//     },
+//     {
+//         id:"20",
+//         district:"Binh Chanh"
+//     },
+//     {
+//         id:"21",
+//         district:"Hoc Mon"
+//     },
+//     {
+//         id:"22",
+//         district:"Cu Chi"
+//     },
+//     {
+//         id:"23",
+//         district:"Can Gio"
+//     },
+//     {
+//         id:"24",
+//         district:"Nha Be"
+//     },
+// ]
